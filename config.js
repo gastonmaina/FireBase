9618f9521f0cc8b75538294fa5f87017ca830786
gastonmaina <115586622+gastonmaina@users.noreply.github.com>
2024-12-06 18:49:53 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 export default {
-    "nombre": "Gaston"
+    "nombreBaseDeDatos": "PendingsList",
+    "ruta1": "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js"
+    "ruta2": "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js"
 }
