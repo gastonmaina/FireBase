125db1383063f6aaadea2c12eaf18f544674c4d9
gastonmaina <115586622+gastonmaina@users.noreply.github.com>
2024-12-10 12:26:58 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
-import { getDatabase, ref, push, onValue, remove } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js';
+import { getDatabase, ref, push, onValue, remove, update } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js';
 
 
 export default {
     initializeApp,
-    getDatabase, ref, push, onValue, remove,
+    getDatabase, 
+    ref, 
+    push, 
+    onValue, 
+    remove, 
+    update,
     databaseURL: "https://hannitahotel-default-rtdb.firebaseio.com/",
     nombreBaseDeDatos: "PendingsList",
     gmaina: "1234"
