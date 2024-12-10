2b807ee0553395990c70d71f6f905a8cab1b9f4a
gastonmaina <115586622+gastonmaina@users.noreply.github.com>
2024-12-10 15:40:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ const btnpass$ = document.getElementById("btnPass")
 const modalLogin$ = document.getElementById("modalLogin")
 const body$ = document.getElementsByName("body")
 
+const email = "gmaina@biomas.net";
+const password = "180301";
+
+config.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Usuario autenticado
+    const user = userCredential.user;
+    console.log("Usuario autenticado:", user);
+  })
+  .catch((error) => {
+    console.error("Error en la autenticación:", error);
+  });
+
 
 btnAgregar$.addEventListener("click", function(){
     const fechaActual = new Date(); 
